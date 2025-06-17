2b3c21102501a9457f09531e7a92e7c824a09c85
Rikatoki <haniszeker@gmail.com>
2025-06-17 09:59:12 -0300

    erro tecnico

--- sistema/menu.py
+++ sistema/menu.py
@@ -1,7 +1,7 @@
 def linha(qnt=30, crtr='-=-'):
     '''
     CRIADOR DE LINHAS\n
-    param qnt: Valor númerico de quantas linhas deseja. (Padrão: 20)\n
+    param qnt: Valor númerico de quantas linhas deseja. (Padrão: 30)\n
     param crtr: Caractere da linha. (Padrão: -=-)
     '''
     return print(f'{crtr}'*qnt)
@@ -10,7 +10,7 @@ def opçoes(*txt,cbç='MENU PRINCIPAL', form=90):
     CRIAÇÃO AUTOMÁTICA DO MENU\n
     param txt: Opções que queira inserir. (obrigatório) (Não há necessidade de numerar elas)\n
     param cbç: Cabeçário das opções. (Padrão: "MENU PRINCIPAL")\n
-    param form: Formatação númerica do cabeçário (Padrão: 60)\n
+    param form: Formatação númerica do cabeçário (Padrão: 90)\n
     return: todas as opções muito bem formatadas
     '''
     linha()
