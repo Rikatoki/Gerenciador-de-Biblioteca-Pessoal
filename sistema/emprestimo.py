93bfe3eca2ec0d80d215f35dcf1f114034d4f984
Rikatoki <haniszeker@gmail.com>
2025-06-24 07:59:41 -0300

    Biblioteca 2.0 (erro)

--- sistema/emprestimo.py
+++ sistema/emprestimo.py
@@ -4,13 +4,15 @@ from datetime import date
 from json import load,dump
 class Emprestimo:
     def __init__(self):
-        self.emprestimos: list[list[Livro]] = []
+        self.emprestimos: list[tuple[Usuario,Livro,date,date]] = []
 
-    def cadastrar(self, usuario:Usuario, livro:Livro,data = date.today()):
+    def cadastrar(self, usuario:Usuario, livro:Livro,data = date.today(), devolucao = None):
         if not livro.emprestado:
-            self.emprestimos.append([usuario, livro, data, False])
+            if devolucao == None:
+                devolucao = False
+            self.emprestimos.append([usuario, livro, data, devolucao])
             livro.emprestado = True
-            return f'Cadastro: Livro: {livro.titulo} emprestado ao {usuario.nome}, email: {usuario.email}.'
+            return f'Cadastro: [Livro: {livro.titulo} emprestado ao {usuario.nome}, email: {usuario.email}.]'
         else:
             return f'O livro {livro.titulo} não está disponível.'
 
@@ -22,6 +24,39 @@ class Emprestimo:
                     l[3] = data
                     l[1].emprestado = False
                     return f'Livro devolvido com sucesso!'
-                return f'Livro já foi devolvido!'
-            return f'Não foi encontrado este registro.'
+        return f'Não foi encontrado este registro, ou o livro já foi devolvido.'
         
+    def emprestimos_ativos(self):
+        ativos = False
+        for e in self.emprestimos:
+            if e[3] == False:
+                print(f'Usuario: {e[0].nome} | Livro: {e[1].titulo} | Data de entrega: {e[2]}')
+                ativos = True
+        return ativos
+
+    def historico_usuario(self,user:int):
+        historico = False
+        for e in self.emprestimos:
+            if e[0].id == user:
+                print(f'Cadastro: [Livro: {e[1].titulo} emprestado ao {e[0].nome}, email: {e[0].email}.]')
+                historico = True
+        return historico
+    
+    def salvar(self):
+        lista = []
+        for e in self.emprestimos:
+            dicionario = {
+                "usuario": e[0],
+                "livro": e[1],
+                "data": e[2],
+                "devolucao": e[3]
+            }
+            lista.append(dicionario)
+        with open('dados/emprestimos.json', 'w',encoding='utf-8') as arq:
+            dump(lista,arq, indent=4)
+
+    def carregar(self):
+        with open('dados/emprestimos.json','r',encoding='utf-8') as arq:
+            dados = load(arq)
+            for d  in dados:
+                self.emprestimos.append([d["usuario"], d["livro"], d["data"], d["devolucao"]])
