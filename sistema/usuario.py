2f44dafe56e91e854eae5f148c3211242eb1a86c
Rikatoki <haniszeker@gmail.com>
2025-06-24 17:24:13 -0300

    Biblioteca 1.6

--- sistema/usuario.py
+++ sistema/usuario.py
@@ -1,17 +1,10 @@
-from os import path
-
+import os
+from json import load, dump
 class Usuario:
-    @classmethod
-    def carregar_id(cls):
-        if path.exists('dados/usuario_id.txt'):
-            with open('dados/usuario_id.txt', 'r') as f:
-                return int(f.read())
-        return 0
-    @classmethod
-    def salvar_id(cls,id):
-        with open('dados/usuario_id.txt', 'w') as f:
-            f.write(str(id))
+    usuarios: list['Usuario'] = []
+    
     def __init__(self, nome:str, email:str, id:int = None):
+        Usuario.carregar()
         if id is not None:
             self.id = id    
         else:
@@ -20,6 +13,8 @@ class Usuario:
             self.id =id
         self.nome = nome
         self.email = email
+        Usuario.usuarios.append(self)
+        Usuario.salvar()
 
     def to_dict(self):
         return {
@@ -30,7 +25,43 @@ class Usuario:
     
     @classmethod
     def from_dict(cls, data:list[dict]):
-        return [cls(**u) for u in data]
+        usuarios = []
+        for d in data:
+            user = cls.__new__(cls)
+            user.id = d["id"]
+            user.nome = d["nome"]
+            user.email = d["email"]
+            usuarios.append(user)
+        return usuarios
+    
+    @classmethod
+    def salvar(cls):
+        usuarios = []
+        for u in cls.usuarios:
+            usuarios.append(u.to_dict())
+        os.makedirs('dados', exist_ok=True)
+        with open('dados/usuarios.json', 'w', encoding='utf-8') as arq:
+            dump(usuarios, arq, indent=2)
+
+    @classmethod
+    def carregar(cls):
+        if os.path.exists('dados/usuarios.json'):
+            with open('dados/usuarios.json', 'r', encoding='utf-8') as arq:
+                dados = load(arq)
+            Usuario.usuarios = Usuario.from_dict(dados)
+    
+    @classmethod
+    def carregar_id(cls):
+        if os.path.exists('dados/usuario_id.txt'):
+            with open('dados/usuario_id.txt', 'r') as f:
+                return int(f.read())
+        return 0
+    
+    @classmethod
+    def salvar_id(cls,id):
+        with open('dados/usuario_id.txt', 'w') as f:
+            f.write(str(id))
     
     def __str__(self):
         return f'ID:{self.id} - [{self.nome}] email: {self.email}'
+    
